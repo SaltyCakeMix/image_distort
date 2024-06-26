5ee5301715775eb7e7efc98385237e8181d39231
SaltyCakeMix <justinle33333@gmail.com>
2024-06-25 19:15:07 -0700

    it works kinda

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import time
+import math
 import cv2
 import numpy as np
 import glfw
@@ -6,83 +8,28 @@ import glfw.GLFW as GLFW
 from OpenGL.GL import *
 from OpenGL.GL import shaders
 import ctypes
+from perlin_noise import PerlinNoise
 
-class Triangle:
-    def __init__(self, shader):
-        # Defines mesh information
-        detail = 10
-        self.vertex_count = (detail + 1)**2  + (detail + 1) // 2
-        self.triangle_count = (2*detail + 1) * detail
-        self.vertices = np.zeros(self.vertex_count*2, dtype=np.float32)
-        self.indices = np.zeros(self.triangle_count*3, dtype=np.int32)
-
-        index = 0
-        for i in range(detail + 1):
-            for j in range(detail + (1 if i%2==0 else 2)):
-                self.vertices[index] = j / detail if i%2==0 else max(min((2*j - 1)/(2*detail), 1.0), 0.0)
-                self.vertices[index + 1] = i / detail
-                index += 2
-
-        index = 0
-        a = np.array([0, detail + 1, detail + 2, 0, detail + 2, 1])
-        for i in range(detail):
-            self.indices[index : index+3] = (a[0:3] if i%2 == 0 else a[3:6] + 1) + detail
-            index += 3
-            quad = a if i%2 == 0 else a + detail + 2
-            for j in range(detail):
-                self.indices[index : index+6] = quad + j
-                index += 6
-            if i%2 == 0:
-                a += 2*detail + 3
-
-        # Buffers data to pass to shaders
-            # VAO information
-        self.vao = glGenVertexArrays(1)
-        glBindVertexArray(self.vao)
-
-            # Vertex information
-        self.vbo = glGenBuffers(1)
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
-        
-            # Index information
-        self.ebo = glGenBuffers(1)
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.ebo)
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.indices.nbytes, self.indices, GL_STATIC_DRAW)
-
-            # Vertex formatting
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 8, ctypes.c_void_p(0))
-        
-        # Reads a texture
-        image = cv2.imread(f'src/miku.jpeg')
-        image = cv2.flip(image, 0)
-        self.width = len(image[0])
-        self.height = len(image)
-        self.texture = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, self.texture)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1 if image.itemsize & 3 else 4)
-        glTexImage2D(
-            GL_TEXTURE_2D, 
-            0, 
-            GL_RGB, 
-            self.width, self.height,
-            0,
-            GL_BGR, 
-            GL_UNSIGNED_BYTE, 
-            image)
-
-    def destroy(self):
-        glDeleteVertexArrays(1, [self.vao])
-        glDeleteBuffers(1, [self.vbo, self.ebo])
-        glDeleteTextures(1, [self.texture])
+def clamp(x) -> float:
+    return min(max(x, 0.0), 1.0)
 
 if __name__ == "__main__":
+    src = 'src/'
+    dst = 'dst/'
+    visible = True
+
+    # Determines how many triangles are in the distortion mesh
+    # A resolution of 100 means there's 100 triangles horizontally and 100 vertically
+    res = 100
+
+    # Determines how large the Perlin noise pattern is
+    # A higher number means more detailed textures
+    zoom = 20
+    noise_coeff = 2.0 / zoom**2
+
+    # Determines how many variations of each image will be created
+    variants = 3
+
     # Sets up glfw
     glfw.init()
     glfw.window_hint(GLFW.GLFW_CONTEXT_VERSION_MAJOR,3)
@@ -95,13 +42,13 @@ if __name__ == "__main__":
         GLFW.GLFW_OPENGL_FORWARD_COMPAT, 
         GLFW.GLFW_TRUE
     )
-    # glfw.window_hint(
-    #    GLFW.GLFW_VISIBLE, 
-    #    GLFW.GLFW_FALSE
-    # )
+    if not visible:
+        glfw.window_hint(
+        GLFW.GLFW_VISIBLE, 
+        GLFW.GLFW_FALSE
+        )
     window = glfw.create_window(1000, 1000, "Window", None, None)
     glfw.make_context_current(window)
-    glClearColor(0.1, 0.2, 0.2, 1)
     
     # Creates the shader
     vertex_shader = shaders.compileShader(
@@ -109,11 +56,13 @@ if __name__ == "__main__":
         #version 330 core
         
         layout (location=0) in vec2 vertex_pos;
+        layout (location=1) in vec2 noise;
+        uniform float noise_coeff;
         out vec2 tex_pos;
 
         void main() {
             tex_pos = vertex_pos;
-            gl_Position = vec4(pow(vertex_pos.x, 2.0)*2.0 - 1.0, pow(vertex_pos.y, 2.0)*2.0 - 1.0, 0.0, 1.0);
+            gl_Position = vec4((max(min(vertex_pos + noise*noise_coeff, 1.0), 0.0))*2.0 - 1.0, 0.0, 1.0);
         }
         """, GL_VERTEX_SHADER
     )
@@ -132,71 +81,125 @@ if __name__ == "__main__":
     )
     shader = shaders.compileProgram(vertex_shader, fragment_shader)
 
-    src = 'src/'
-    dst = 'dst/'
+    start_time = time.time()
+    # Defines mesh information
+    vertex_count = (res + 1)**2  + (res + 1) // 2
+    triangle_count = (2*res + 1) * res
+    vertices = np.zeros(vertex_count*4, dtype=np.float32)
+    indices = np.zeros(triangle_count*3, dtype=np.int32)
 
-    t = Triangle(shader)
+        # Creates vertices
+    index = 0
+    cols = (res + 1)*4
+
+            # Precomputes rows
+    row_a = np.zeros(cols)
+    row_a[::4] = [j / res for j in range(res + 1)]
+    row_b = np.zeros(cols + 1)
+    row_b[::4] = [clamp((2*j - 1)/(2*res)) for j in range(res + 2)]
+
+    for i in range(res + 1):
+        y = i / res
+        vertices[index:index + cols + i%2] = row_a if i%2==0 else row_b
+        vertices[index + 1: index + cols + i%2 + 1:4] = y
+        index += (res + 1 + i%2)*4
+
+        # Creates indices
+    index = 0
+    a = np.array([0, res + 1, res + 2, 0, res + 2, 1])
+    for i in range(res):
+        indices[index : index+3] = a[0:3] + res if i%2 == 0 else a[3:6]
+        index += 3
+        quad = a + i%2
+        for j in range(res):
+            indices[index : index+6] = quad + j
+            index += 6
+        a += res + 1 + i%2
+
+    # Buffers data to pass to shaders
+        # VAO information
+    vao = glGenVertexArrays(1)
+    glBindVertexArray(vao)
+
+        # Vertex information
+    vbo = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
     
-    glUseProgram(shader)
+        # Index information
+    ebo = glGenBuffers(1)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
+
+        # Vertex formatting
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 16, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(1)
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 16, ctypes.c_void_p(8))
+    
+        # Creates a texture object
+    texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
 
     # Main loop
-    while not glfw.window_should_close(window):
-        if glfw.get_key(window, GLFW.GLFW_KEY_ESCAPE) \
-            == GLFW.GLFW_PRESS:
-            break
-        glfw.poll_events()
+    vertex_coordinates = list(zip(vertices[::4], vertices[1::4]))
+    glUseProgram(shader)
+    glUniform1f(glGetUniformLocation(shader, 'noise_coeff'), noise_coeff)
+    for path in os.listdir(src):
+        # Reads a texture
+        image = cv2.imread(f'{src}{path}')
+        image = cv2.flip(image, 0)
+        width = len(image[0])
+        height = len(image)
         
-        glClear(GL_COLOR_BUFFER_BIT)
-        glBindVertexArray(t.vao)
-        glDrawElements(GL_TRIANGLES, len(t.indices), GL_UNSIGNED_INT, None)
-        glfw.swap_buffers(window)
-    
-    output = np.zeros((1000, 1000, 3), np.uint8)
-    glReadPixels(0, 0, 1000, 1000, GL_BGR, GL_UNSIGNED_BYTE, output)
-    output = cv2.flip(output, 0)
-    cv2.imwrite('dst/test.png', output)
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1 if image.itemsize & 3 else 4)
+        glTexImage2D(
+            GL_TEXTURE_2D, 
+            0, 
+            GL_RGB, 
+            width, height,
+            0,
+            GL_BGR, 
+            GL_UNSIGNED_BYTE, 
+            image)
+        
+        # Updates the rendering size
+        glfw.set_window_size(window, width, height)
+        glViewport(0, 0, width, height)
+
+        for version in range(variants):
+            # Creates new noise
+            noise_sample_x = PerlinNoise(octaves=zoom)
+            noise_sample_y = PerlinNoise(octaves=zoom)
+
+            vertices[2::4] = [math.ceil(x%1) * noise_sample_x([x, y]) for x, y in vertex_coordinates]
+            vertices[3::4] = [math.ceil(y%1) * noise_sample_y([x, y]) for x, y in vertex_coordinates]
+            glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_DYNAMIC_DRAW)
+            
+            glBindVertexArray(vao)
+            glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, None)
+        
+            # Saves image
+            output = np.zeros((height, width, 3), np.uint8)
+            glReadPixels(0, 0, width, height, GL_BGR, GL_UNSIGNED_BYTE, output)
+            output = cv2.flip(output, 0)
+            path_extension = path.split('.')
+            cv2.imwrite(f'{dst}{path_extension[0]}_{version}.{path_extension[1]}', output)
+
+            # Displays image to window
+            if visible:
+                glfw.swap_buffers(window)
+    print(time.time() - start_time)
 
     # Clean-up
-    t.destroy()
+    glDeleteVertexArrays(1, [vao])
+    glDeleteBuffers(2, [vbo, ebo])
+    glDeleteTextures(1, [texture])
     glDeleteShader(vertex_shader)
     glDeleteShader(fragment_shader)
     glDeleteProgram(shader)
     glfw.destroy_window(window)
     glfw.terminate()
-
-# Determines how many vertices are in the distortion mesh
-# A resolution of 100 means there's 100 vertices horizontally and 100 vertices vertically
-#res = 50
-
-# Determines how large the Perlin noise pattern is
-# A higher number means more detailed textures
-#scale = 10
-
-#for path in os.listdir(src):
-#    break
-
-'''
-(2) 3 2 2 3
-
-0 3 4   0 4 1           3 7 4
-1 4 5   0 5 2   4 7 8   4 8 5
-2 5 6           5 8 9   5 9 6
-
-
-(3) 4 3 3 4 4 3
-0 4 5   0 5 1           4 9 5   9 13 14     9 14 10
-1 5 6   1 6 2   5 9 10  5 10 6  10 14 15    10 15 11
-2 6 7   2 7 3   6 10 11 6 11 7  11 15 16    11 16 12
-3 7 8           7 11 12 7 12 8  12 16 17
-
-
-(4) 5 4 4 5 5 4 4 5
-0 5 6   0 6 1               5 11 6  11 16 17    11 17 12                16 22 17
-1 6 7   1 7 2   6 11 12     6 12 7  12 17 18    12 18 13    17 22 23    17 23 18
-2 7 8   2 8 3   7 12 13     7 13 8  13 18 19    13 19 14    18 23 24    18 24 19
-3 8 9   3 9 4   8 13 14     8 14 9  14 19 20    14 20 15    19 24 25    19 25 20
-4 9 10          9 14 15     9 15 10 15 20 21                20 25 26    20 26 21
-
-(5)
-0 6 7   0 7 1   7 13 14     6 13 7  13 19 20    13 20 14    20 26 27    19 26 20    26 32 33    26 33 27
-'''
